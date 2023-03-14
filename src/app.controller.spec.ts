cffa26aa9a4a16733eb58a1e1915a5a52c0e9902
GokulnathP <4gokul2000@gmail.com>
2023-03-14 13:06:12 +0530

    feat: [TMSF-20] - Configure test coverage

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,15 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
+  const configurations = { APP_NAME: 'TAMASHFLIX' };
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ load: [() => configurations] })],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -16,7 +19,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World from TAMASHFLIX Microservice!');
     });
   });
 });
