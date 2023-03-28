dd13d458f0be631b665986f35bef67ba2ede1332
Rohit Shrivastava <rohitxsh@gmail.com>
2023-03-28 19:28:57 +0530

    fix: [TMSF-14] - test issues

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,20 @@
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import appConfig from '../config/app.config';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
+  const envVariables = { APP_CODE: 'TMSF' };
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ load: [() => envVariables, appConfig] }), JwtModule.register({}), PrismaModule],
       controllers: [AuthController],
+      providers: [AuthService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,11 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import appConfig from '../config/app.config';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('AuthService', () => {
+  const envVariables = { APP_CODE: 'TMSF' };
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ load: [() => envVariables, appConfig] }), JwtModule.register({}), PrismaModule],
       providers: [AuthService],
     }).compile();
 
